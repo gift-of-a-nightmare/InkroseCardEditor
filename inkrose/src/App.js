0071fe2ac2e6a7c834a50ce99b82dd9971cdb320
Lynn! <11156411+WickedMerryAstral@users.noreply.github.com>
2023-05-01 21:50:37 +0200

    basic layout, waiting for feedback .w.

--- inkrose/src/App.js
+++ inkrose/src/App.js
@@ -2,8 +2,15 @@ import './App.css';
 
 function App() {
   return (
-    <div className="app">
-
+    <div className='app'>
+      <div className='container'>
+        <div className='container-item edit'>
+          <p>VALUES: Name, Cost, Art, Type, Sub-type, (power / toughness), (sigils), text1, text2, flavour text, card ID</p>
+        </div>
+        <div className='container-item preview'>
+          <img src='/frame.png' alt="card frame"/>
+        </div>
+      </div>
     </div>
   );
 }
